5d1271dcf7f4849ab9bcb77b7c6a3b9faa29c381
M1L0J05 <moyo.gsxr@gmail.com>
2023-12-31 18:17:14 +0100

    Reutilizando componentes

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -1,22 +1,34 @@
-# chatapp.py
-
 import reflex as rx
+
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(question, text_align="right"),
+        rx.box(answer, text_align="left"),
+        margin_y="1em",
+    )
+
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer)
+            for question, answer in qa_pairs
+        ]
+    )
 
 
 def index() -> rx.Component:
-    return rx.container(
-        rx.box(
-            "What is Reflex?",
-            # The user's question is on the right.
-            text_align="right",
-        ),
-        rx.box(
-            "A way to build web apps in pure Python!",
-            # The answer is on the left.
-            text_align="left",
-        ),
-    )
-
+    return rx.container(chat())
 
 # Add state and page to the app.
 app = rx.App()
