a53d7a99b7613eefec1f2f1fe4492e21a3f82c83
M1L0J05 <moyo.gsxr@gmail.com>
2023-12-31 19:03:18 +0100

    Añadiendo estilos

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -1,9 +1,22 @@
 import reflex as rx
 
+from . import style
+
 def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="right"),
-        rx.box(answer, text_align="left"),
+        rx.box(
+            rx.text(
+                question,
+                style=style.question_style
+            ), 
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(
+                answer, 
+                style=style.answer_style,
+            ), 
+            text_align="left"),
         margin_y="1em",
     )
 
@@ -29,8 +42,15 @@ def chat() -> rx.Component:
 
 def action_bar() -> rx.Component:
     return rx.hstack(
-        rx.input(placeholder="Ask a question"),
-        rx.button("Ask"),
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        rx.button(
+            "Ask",
+            style=style.button_style,
+        ),
+        
     )
 
 
